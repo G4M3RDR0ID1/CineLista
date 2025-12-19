9057407796bc5b9443df874f8eec367eabe95c06
G4M3RDR0ID1 <gamerdroid33@hotmail.com>
2025-12-18 23:00:36 -0300

    CineLista V1.0

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { getTrendindMovies } from "@/lib/api/tmdb";
+import Grid from "./components/Grid";
+import Title from "./components/Title"
+
+
+export default async function Home() {
+  const filmes = await getTrendindMovies();
+  return (
+    <>
+    <Title title="Filmes em destaque"/>
+    <Grid filmes={filmes}/>
+    </>
+  );
+}
